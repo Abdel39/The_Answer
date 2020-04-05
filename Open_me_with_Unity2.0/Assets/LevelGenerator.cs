7d3debb7ea233be2200939e7c0b4a6d15949ba94
Maximilien22 <55790623+Maximilien22@users.noreply.github.com>
2020-04-05 12:03:38 +0200

    j ai ajouté des scripts pour l editeur de niveau

---
+++ Open_me_with_Unity2.0/Assets/LevelGenerator.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+    public Texture2D map;
+
+    public ColorToPrefab[] colorMappings;
+
+    // Use this for initialization
+    void Start () {
+        GenerateLevel();
+    }
+
+    void GenerateLevel ()
+    {
+        for (int x = 0; x < map.width; x++)
+        {
+            for (int y = 0; y < map.height; y++)
+            {
+                GenerateTile(x, y);
+            }
+        }
+    }
+
+    void GenerateTile (int x, int y)
+    {
+        Color pixelColor = map.GetPixel(x, y);
+
+        if (pixelColor.a == 0)
+        {
+            // le pixel est transparent donc on fait rien
+            return;
+        }
+
+        foreach (ColorToPrefab colorMapping in colorMappings)
+        {
+            if (colorMapping.color.Equals(pixelColor))
+            {
+                // le pixel a une couleur egale a celle d un de nos couleur predefini dans colorMappings,
+                // donc on creer l element associé
+                Vector2 position = new Vector2(x, y);
+                Instantiate(colorMapping.prefab, position, Quaternion.identity, transform);
+            }
+        }
+    }
+	
+}
---
+++ Open_me_with_Unity2.0/Assets/script/ColorToPrefab.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[System.Serializable]// ca je sais pas exactement comment ca marche, mais c'est necessaire pour que ca y dessine bien
+public class ColorToPrefab {
+
+    public Color color;
+    public GameObject prefab;
+
+}
