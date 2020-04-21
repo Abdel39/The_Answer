30bfd07ab7b292713f8e2609d473cb58998bf47b
Samuel Bonnet <samuel.bonnet@epita.fr>
2020-04-21 17:43:51 +0200

    ajout teleporteur

---
+++ Open_me_with_Unity2.0/Assets/script/teleporteur.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class teleporteur : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D info)
+    {
+        playermoves player = info.GetComponent<playermoves>();
+        if (info!=null)
+        {
+            SceneManager.LoadScene("Mars boss Battle");
+            Debug.Log("boom");
+        }
+            
+        
+    }
+}
