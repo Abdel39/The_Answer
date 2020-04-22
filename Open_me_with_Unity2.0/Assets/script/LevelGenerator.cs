afedc3f481d399f57204f013c436cccb75d7cd0b
Maximilien22 <55790623+Maximilien22@users.noreply.github.com>
2020-04-22 12:43:13 +0200

    modification du hub

--- Open_me_with_Unity2.0/Assets/script/LevelGenerator.cs
+++ Open_me_with_Unity2.0/Assets/script/LevelGenerator.cs
@@ -1,39 +1,117 @@
 ﻿using System.IO;
 using System.Net;
+using System.Collections;
+using System.Collections.Generic;
 using System.Net.Mime;
 using UnityEngine;
 using UnityEngine.Experimental.PlayerLoop;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
-public class LevelGenerator : MonoBehaviour {
+public class LevelGenerator : MonoBehaviour
+{
 
-    public Texture2D map ;
-        
-
+    private static Texture2D map;
     public ColorToPrefab[] colorMappings;
+    public Text debugText;
 
-    
-    void Start ()
+
+
+    public Dropdown dropdown;
+    private static string my_path;
+    private List<string> listLvl = new List<string>();
+
+    public bool fromEditedScene;
+
+    // Start is called before the first frame update
+    void Start()
     {
-        FetchTheImage();
-        GenerateLevel();
-    }
-    
-    void FetchTheImage()
-    {
-        /*string path = "C:\\Users\\futil\\Desktop\\New_Piskel.png";
-        // y faut juste que le path nous ramene a un dossier "lvlImage" que l on creera et qui contiendra l image du nouveau niveau
-        byte[] photoInBytes = File.ReadAllBytes(path);
-        bool mapAvaiable = this.map.LoadImage(photoInBytes);
-        if (!mapAvaiable)
+        if (fromEditedScene)
         {
-            //afficher un message d erreur
-        }*/
-      
+            Debug.Log("depuis editScene");
+            FetchTheImage();
+            GenerateLevel();
+
+        }
+        else
+        {
+            InitDropDown();
+            Debug.Log("pas depuis editScene");
+        }
+
     }
-    void GenerateLevel ()
+
+    private void InitDropDown()
     {
+        Debug.Log("j init ma dropdown");
+
+        string path = Application.dataPath + @"/lvlEditor/";
+
+        string[] my_list = Directory.GetFiles(path);
+
+        foreach (string item in my_list)
+        {
+            FileInfo fileInfo = new FileInfo(item);
+
+
+            int longueur = fileInfo.Name.Length;
+
+            Debug.Log("j ai : " + fileInfo.Name.Substring(0, longueur - 4));
+
+
+            listLvl.Add(fileInfo.Name.Substring(0, longueur - 4));
+        }
+
+
+        dropdown.AddOptions(listLvl);
+
+        Debug.Log("je les ai ajouté a ma liste et a ma drop box");
+
+
+
+
+    }
+
+    public void onDropDownClick(int index)
+    {
+
+
+        my_path = Application.dataPath + @"/lvlEditor/" + listLvl[index] + ".png";
+
+        Debug.Log("on vient de clické sur drop down, index : " + index + " ce qui est : " + my_path);
+
+
+    }
+
+    public void onLaunchEditSceneClicked()
+    {
+        SceneManager.LoadScene("EditedLvl");
+    }
+
+
+    private void FetchTheImage()
+    {
+        Debug.Log("fetch the image");
+       
+         
+            Debug.Log("je fetch l image");
+            Debug.Log("son path : " + my_path);
+            byte[] photoInBytes = File.ReadAllBytes(my_path);// j ai un probleme sur 
+            bool mapAvaiable = map.LoadImage(photoInBytes);//ces deux lignes
+        Debug.Log("j ai une erreur : " + mapAvaiable);
+       
+
+        debugText.text = "l image est : " + my_path;
+
+    }
+
+    private void GenerateLevel()
+    {
+        Debug.Log("generate level");
+
+
+
         for (int x = 0; x < map.width; x++)
         {
             for (int y = 0; y < map.height; y++)
@@ -43,11 +121,11 @@ public class LevelGenerator : MonoBehaviour {
         }
     }
 
-    void GenerateTile (int x, int y)
+    private void GenerateTile(int x, int y)
     {
         Color pixelColor = map.GetPixel(x, y);
 
-        if (pixelColor.a == 0 || pixelColor.Equals(new Color(255,255,255)))
+        if (pixelColor.a == 0 || pixelColor.Equals(new Color(255, 255, 255)))
         {
             // le pixel est transparent ou blanc donc on fait rien
             return;
@@ -64,5 +142,5 @@ public class LevelGenerator : MonoBehaviour {
             }
         }
     }
-	
+
 }
