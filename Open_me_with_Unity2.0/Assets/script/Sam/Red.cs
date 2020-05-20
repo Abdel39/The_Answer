e5501a5b27a532f5217e0aac9c4d0462f0f132c9
Samuel Bonnet <samuel.bonnet@epita.fr>
2020-05-20 18:23:44 +0200

    ajout plateforme on/off

---
+++ Open_me_with_Unity2.0/Assets/script/Sam/Red.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Red : MonoBehaviour
+{
+    private bool on;
+    private Collider2D box;
+    public Switch Switch;
+    public Animator animator;
+    void Start()
+    {
+        on = false;
+        box = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        on = Switch.On;
+
+        if (on)
+        {
+            animator.SetBool("Off", true);
+            
+            box.enabled = true;
+        }
+        else
+        {
+            animator.SetBool("Off",false);
+            box.enabled = false;
+        }
+        
+    }
+}
---
+++ Open_me_with_Unity2.0/Assets/script/Sam/blue.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blue : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private bool on;
+    private Collider2D box;
+    public Switch Switch;
+    public Animator animator;
+    void Start()
+    {
+        on = false;
+        box = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        on = !Switch.On;
+
+        if (on)
+        {
+            animator.SetBool("Off", true);
+            
+            box.enabled = true;
+        }
+        else
+        {
+            animator.SetBool("Off",false);
+            box.enabled = false;
+        }
+        
+    }
+}
