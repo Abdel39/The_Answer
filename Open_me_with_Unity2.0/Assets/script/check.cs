aa4f41da02b909779c103f2773f4b700acbccdfb
Samuel Bonnet <samuel.bonnet@epita.fr>
2020-05-21 15:35:13 +0200

    up lvl2

--- Open_me_with_Unity2.0/Assets/script/check.cs
+++ Open_me_with_Unity2.0/Assets/script/check.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class check : MonoBehaviour
 {
     private GameObject character;
+
+    private bool test = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,14 +20,7 @@ public class check : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.collider.tag == "IsGround")
-        {
-            character.GetComponent<PlayerMotor>().isgrounded = true;
-        }
-    }
-
+    
     private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.collider.tag == "IsGround")
@@ -33,4 +28,14 @@ public class check : MonoBehaviour
             character.GetComponent<PlayerMotor>().isgrounded = false;
         }
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "IsGround")
+        {
+            test = true;
+            character.GetComponent<PlayerMotor>().isgrounded = true;
+        }
+    }
+
+    
 }
