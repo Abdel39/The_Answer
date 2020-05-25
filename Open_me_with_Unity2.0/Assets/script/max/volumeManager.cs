159558c37e388a702ab448d1081d1c53b78a8912
maximilien <maximilien.charmetant@epita.fr>
2020-05-25 16:10:10 +0200

    volume pour de vrai mdr

---
+++ Open_me_with_Unity2.0/Assets/script/max/volumeManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class volumeManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+    private static float currentVolume=10.0f;
+    public Slider slider;
+    public bool IamSettingSlider=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (IamSettingSlider && slider !=null)
+        {
+            slider.value = audioSource.volume;
+        }
+
+        audioSource.volume = currentVolume;
+    }
+
+    public void setVolumeWithSlider()
+    {
+        currentVolume = slider.value;
+        audioSource.volume = currentVolume;
+    }
+
+    
+}
