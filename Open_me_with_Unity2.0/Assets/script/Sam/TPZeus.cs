3ff6b0a0336b3251034c274f7fdfd4eee645129e
Samuel Bonnet <samuel.bonnet@epita.fr>
2020-05-25 18:13:48 +0200

    niveau 4

---
+++ Open_me_with_Unity2.0/Assets/script/Sam/TPZeus.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class TPZeus : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D info)
+    {
+        playermoves player = info.GetComponent<playermoves>();
+        if (info!=null)
+        {
+            SceneManager.LoadScene("Zeus boss Battle");
+            Debug.Log("boom");
+        }
+            
+        
+    }
+}
